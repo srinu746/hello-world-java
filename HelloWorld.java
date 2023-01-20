0fcc6d29124515d1d772f4cb018ef453096c8c7d
srinu746 <119294210+srinu746@users.noreply.github.com>
2023-01-20 16:27:35 +0530

    new update

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     System.out.println("Hello world!");
      System.out.println("Hello world!");
      System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
   }
 }
