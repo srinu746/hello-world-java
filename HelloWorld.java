36f75f1d062f550d61797e896eaf0de5bb3ebb71
srinu746 <119294210+srinu746@users.noreply.github.com>
2023-01-20 16:35:12 +0530

    webb

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
      System.out.println("Hello world!");
-     System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
   }
 }
